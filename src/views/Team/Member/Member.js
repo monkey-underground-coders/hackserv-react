0ef9019d526cef069cc44d6f0abbd9b260967955
Alexandr Lobashov <viper.489@inbox.ru>
2021-08-12 15:59:37 +0300

    recovery point

--- src/views/Team/Member/Member.js
+++ src/views/Team/Member/Member.js
@@ -4,7 +4,7 @@ import { getUserByIdSelector } from '@redux/users';
 import { useParamSelector } from '@utils/hooks';
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
+const classes = makeStyles((theme) => ({
     root: {
         width: '100%',
     },
@@ -21,8 +21,8 @@ const Member = ({id}) => {
     const lastName = user.lastName;
 
     return (
-        <div className="root">
-            <div className='name'>{`${firstName} ${middleName} ${lastName}`}</div>
+        <div className={classes.root}>
+            <div className={classes.name}>{`${firstName} ${middleName} ${lastName}`}</div>
         </div>    
     );
 };
